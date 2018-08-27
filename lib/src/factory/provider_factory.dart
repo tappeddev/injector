b212b3809ab3d39f4f848c44dc97d8ebcee22063
Stefan <ste.schaller@gmx.de>
2018-08-27 07:21:32 +0200

    Typo

--- lib/src/factory/provider_factory.dart
+++ lib/src/factory/provider_factory.dart
@@ -2,7 +2,7 @@ import 'package:injector/injector.dart';
 import 'package:injector/src/factory/factory.dart';
 
 /// This Factory does lazy instantiation of [T] and
-/// always returns a new instance builder by the [builder]
+/// always returns a new instance built by the [builder]
 class ProviderFactory<T> implements Factory<T> {
   @override
   Builder<T> builder;
