23569910830638fecd74e777ff40b7d03fd9ca4e
Stefan <ste.schaller@gmx.de>
2019-03-16 00:47:15 +0100

    feat/extend_injector add tests

--- test/injector_test.dart
+++ test/injector_test.dart
@@ -133,4 +133,17 @@ void main() {
     expect(engine1, TypeMatcher<Engine>());
     expect(engine2, TypeMatcher<Engine>());
   });
+
+
+  test("override a dependencies", () {
+
+    injector.registerDependency<Engine>((injector) => Engine()..capacity = "1");
+
+    injector.registerDependency<Engine>((injector) => Engine()..capacity = "2", override: true);
+
+    var engine = injector.getDependency<Engine>();
+
+    expect(engine.capacity, "2");
+  });
+
 }
