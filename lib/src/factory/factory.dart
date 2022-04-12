22c39e03c878ccbd6362c32302acd6661ff2586d
Julian Bissekkou <julian.bissekkou@tapped.dev>
2022-04-12 15:12:10 +0200

    fix docs

--- lib/src/factory/factory.dart
+++ lib/src/factory/factory.dart
@@ -1,15 +1,13 @@
 /// Gets registered at the injector an then gets called by the injector to
 /// instantiate the dependency and all of its dependencies.
 ///
-/// Use the inner [injector] to get the dependencies that are required to
-/// instantiate the new dependency.
 ///
 /// Example:
 /// ```dart
-/// var myInjector = Injector();
+/// final myInjector = Injector();
 ///
-/// myInjector.registerDependency<Car>((Injector innerInjector) {
-///     var engine = innerInjector.getDependency<Engine>();
+/// myInjector.registerDependency<Car>(() {
+///     var engine = myInjector.getDependency<Engine>();
 ///     return CarImpl(engine: engine);
 /// });
 /// ```
